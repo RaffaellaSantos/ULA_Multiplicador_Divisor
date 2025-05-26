c64b1f099960d26e3b7ad370876e485c26259846
Raffaella_SF <adraffaella6@gmail.com>
2025-05-26 14:15:01 -0400

    Multiplicador com ULA e MUX 8b e 16b

---
+++ Multiplicador/mux8x1_8b.v
@@ -0,0 +1,21 @@
+module mux8x1_8b (
+	input [7:0] a, b,
+	input x, y, z,
+	output reg [7:0] s
+);
+		
+	always@*
+	begin
+		case ({x, y, z})
+			3'b000: s = a + b;
+			3'b001: s = a - b;
+			3'b010: s = a << b;
+			3'b011: s = a >> b;
+			3'b100: s = a & b;
+			3'b101: s = a | b;
+			3'b110: s = a ^ b;
+			3'b111: s = ~a;
+		endcase
+	end
+
+endmodule
